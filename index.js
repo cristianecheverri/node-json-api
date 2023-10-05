74eddd70975dc20c2444c0f0f8df4487f03092c2
Carlos Castañeda <carloscastaneda2323@gmail.com>
2023-10-04 20:21:16 -0500

    Editar, Mostrar un elemento y eliminar

--- index.js
+++ index.js
@@ -54,6 +54,62 @@ app.post('/pets', (req, res) => {
     }
 });
 
+//Obtener una sola mascota
+app.get('/pets/:id', (req, res) => {
+    console.log(req.params.id);
+    //Guardar el ID
+    const id = req.params.id
+    //Leer el contenido del archivo
+    const pets = readFile(FILE_NAME)
+    // Buscar la mascota con el ID que recibimos
+    const petFound = pets.find(pet => pet.id === id )
+    if(!petFound){// Si no se encuentra la mascota con ese ID
+        res.status(404).json({'ok': false, message:"Pet not found"})
+        return;
+    }
+    res.json({'ok': true, pet: petFound});
+})
+
+//Actualizar una mascota
+app.put('/pets/:id', (req, res) => {
+    console.log(req.params.id);
+    //Guardar el ID
+    const id = req.params.id
+    //Leer el contenido del archivo
+    const pets = readFile(FILE_NAME)
+    // Buscar la mascota con el ID que recibimos
+    const petIndex = pets.findIndex(pet => pet.id === id )
+    if( petIndex < 0 ){// Si no se encuentra la mascota con ese ID
+        res.status(404).json({'ok': false, message:"Pet not found"});
+        return;
+    }
+    let pet = pets[petIndex]; //Sacar del arreglo
+    pet = { ...pet, ...req.body  };
+    pets[petIndex] = pet; //Poner la mascota en el mismo lugar
+    writeFile(FILE_NAME, pets);
+    //Si la mascota existe, modificar sus datos y almacenarlo nuevamente
+    res.json({'ok': true, pet: pet});
+})
+
+//Eliminar una mascota
+app.delete('/pets/:id', (req, res) => {
+    console.log(req.params.id);
+    //Guardar el ID
+    const id = req.params.id
+    //Leer el contenido del archivo
+    const pets = readFile(FILE_NAME)
+    // Buscar la mascota con el ID que recibimos
+    const petIndex = pets.findIndex(pet => pet.id === id )
+    if( petIndex < 0 ){// Si no se encuentra la mascota con ese ID
+        res.status(404).json({'ok': false, message:"Pet not found"});
+        return;
+    }
+    //Eliminar la mascota que esté en la posición petIndex
+    pets.splice(petIndex, 1);
+    writeFile(FILE_NAME, pets)
+    res.json({'ok': true});
+})
+
 app.listen(3000, () => {
     console.log(`Server is running on http://localhost:3000`)
 });
