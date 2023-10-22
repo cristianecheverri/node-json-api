7d68aa3248e335edc9b8de0c22142d7d8ea0aec0
Cristian Camilo Echeverri <crisechevrri@gmail.com>
2023-10-21 19:46:31 -0500

    fix(index)changes with variable names and gitignore files

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Librerías externas
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuid_v4 } = require('uuid');
 
 // Módulos internos
 const { readFile, writeFile } = require('./src/files');
 
 const app = express();
-const FILE_NAME = './db/pets.txt';
+const FILE_NAME = './db/books.txt';
+const PORT = 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -34,87 +34,87 @@ app.get('/read-file', (req, res) => {
 });
 
 // API
-// Listar Mascotas
-app.get('/pets', (req, res)=>{
+// Listar Libros
+app.get('/books', (req, res) => {
     const data = readFile(FILE_NAME);
     res.json(data);
 })
 
-//Crear Mascota
-app.post('/pets', (req, res) => {
+//Crear Libro
+app.post('/books', (req, res) => {
     try {
-        //Leer el archivo de mascotas
+        //Leer el archivo de libros
         const data = readFile(FILE_NAME);
-        //Agregar la nueva mascota (Agregar ID)
-        const newPet = req.body;
-        newPet.id = uuidv4();
-        console.log(newPet)
-        data.push(newPet);
+        //Agregar el nuevo libro (Agregar ID)
+        const newBook = req.body;
+        newBook.id = uuid_v4();
+        console.log(newBook)
+        data.push(newBook);
         // Escribir en el archivo
         writeFile(FILE_NAME, data);
-        res.json({ message: 'La mascota fue creada con éxito' });
+        res.json({ message: 'El libro fue creado con éxito' });
     } catch (error) {
         console.error(error);
-        res.json({ message: 'Error al almacenar la mascota' });
+        res.json({ message: 'Error al almacenar el libro' });
     }
 });
 
-//Obtener una sola mascota
-app.get('/pets/:id', (req, res) => {
+//Obtener un solo libro
+app.get('/books/:id', (req, res) => {
     console.log(req.params.id);
     //Guardar el ID
     const id = req.params.id
     //Leer el contenido del archivo
-    const pets = readFile(FILE_NAME)
-    // Buscar la mascota con el ID que recibimos
-    const petFound = pets.find(pet => pet.id === id )
-    if(!petFound){// Si no se encuentra la mascota con ese ID
-        res.status(404).json({'ok': false, message:"Pet not found"})
+    const books = readFile(FILE_NAME)
+    // Buscar el libro con el ID que recibimos
+    const bookFound = books.find(book => book.id === id)
+    if (!bookFound) {// Si no se encuentra el libro con ese ID
+        res.status(404).json({ 'ok': false, message: "Book not found" })
         return;
     }
-    res.json({'ok': true, pet: petFound});
+    res.json({ 'ok': true, book: bookFound });
 })
 
-//Actualizar una mascota
-app.put('/pets/:id', (req, res) => {
+//Actualizar un libro
+app.put('/books/:id', (req, res) => {
     console.log(req.params.id);
     //Guardar el ID
     const id = req.params.id
     //Leer el contenido del archivo
-    const pets = readFile(FILE_NAME)
-    // Buscar la mascota con el ID que recibimos
-    const petIndex = pets.findIndex(pet => pet.id === id )
-    if( petIndex < 0 ){// Si no se encuentra la mascota con ese ID
-        res.status(404).json({'ok': false, message:"Pet not found"});
+    const books = readFile(FILE_NAME)
+    // Buscar el libro con el ID que recibimos
+    const bookIndex = books.findIndex(book => book.id === id)
+    if (bookIndex < 0) {// Si no se encuentra el libro con ese ID
+        res.status(404).json({ 'ok': false, message: "Book not found" });
         return;
     }
-    let pet = pets[petIndex]; //Sacar del arreglo
-    pet = { ...pet, ...req.body  };
-    pets[petIndex] = pet; //Poner la mascota en el mismo lugar
-    writeFile(FILE_NAME, pets);
-    //Si la mascota existe, modificar sus datos y almacenarlo nuevamente
-    res.json({'ok': true, pet: pet});
+    let book = books[bookIndex]; //Sacar del arreglo
+    book = { ...book, ...req.body };
+    books[bookIndex] = book; //Poner el libro en el mismo lugar
+    writeFile(FILE_NAME, books);
+    //Si el libro existe, modificar sus datos y almacenarlo nuevamente
+    res.json({ 'ok': true, book: book });
 })
 
-//Eliminar una mascota
-app.delete('/pets/:id', (req, res) => {
+//Eliminar un libro
+app.delete('/books/:id', (req, res) => {
     console.log(req.params.id);
     //Guardar el ID
     const id = req.params.id
     //Leer el contenido del archivo
-    const pets = readFile(FILE_NAME)
-    // Buscar la mascota con el ID que recibimos
-    const petIndex = pets.findIndex(pet => pet.id === id )
-    if( petIndex < 0 ){// Si no se encuentra la mascota con ese ID
-        res.status(404).json({'ok': false, message:"Pet not found"});
+    const books = readFile(FILE_NAME)
+    // Buscar el libro con el ID que recibimos
+    const bookIndex = books.findIndex(book => book.id === id)
+    if (bookIndex < 0) {// Si no se encuentra el libro con ese ID
+        res.status(404).json({ 'ok': false, message: "Book not found" });
         return;
     }
-    //Eliminar la mascota que esté en la posición petIndex
-    pets.splice(petIndex, 1);
-    writeFile(FILE_NAME, pets)
-    res.json({'ok': true});
+    //Eliminar el libro que esté en la posición petIndex
+    books.splice(bookIndex, 1);
+    writeFile(FILE_NAME, books)
+    res.json({ 'ok': true });
 })
 
-app.listen(3000, () => {
-    console.log(`Server is running on http://localhost:3000`)
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
 });
